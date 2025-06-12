d822e5f15d211d6d388f7e084bcf84cf36d15e0c
syauqiarata <145372723+syauqiarata@users.noreply.github.com>
2025-06-12 20:31:13 +0800

    Add .next/static/chunks/src_app_kriteria_page_tsx_aa707852._.js 1749731471887

---
+++ .next/static/chunks/src_app_kriteria_page_tsx_aa707852._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c06c1b8c._.js",
+  "static/chunks/src_885d87ce._.js"
+],
+    source: "dynamic"
+});
